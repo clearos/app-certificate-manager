09bc8be0b608f3e029c8159880127b28c98136ee
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-17 23:49:06 +0000

    - Disabled user certificates policies if not installed

--- controllers/certificate_manager.php
+++ controllers/certificate_manager.php
@@ -65,7 +65,7 @@ class Certificate_Manager extends ClearOS_Controller
 
         $views = array('certificate_manager/certificate');
 
-        if (!($this->session->userdata('wizard')))
+        if (!($this->session->userdata('wizard')) && clearos_app_installed('user_certificates'))
             $views[] = 'certificate_manager/policy';
 
         $this->page->view_forms($views, lang('certificate_manager_app_name'));
