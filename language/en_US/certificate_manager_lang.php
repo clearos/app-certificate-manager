33d28f531b3787389809d8979dfda70b545583ca
Ben Chambers <bchambers@clearfoundation.com>
2012-08-14 17:41:11 +0000

    - Fixed typo

--- language/en_US/certificate_manager_lang.php
+++ language/en_US/certificate_manager_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['certificate_manager_app_name'] = 'Certificate Manager';
-$lang['certificate_manager_app_description'] = 'Security certificates are an industry standard for encrypting information sent over a network, providing authentication and enabling digital signatures.  The Certificate Manager app provides the ability create, manage and deploy security certificates to users and apps.';
+$lang['certificate_manager_app_description'] = 'Security certificates are an industry standard for encrypting information sent over a network, providing authentication and enabling digital signatures.  The Certificate Manager app provides the ability to create, manage and deploy security certificates to users and apps.';
 $lang['certificate_manager_certificate_manager'] = 'Certificate Manager';
 $lang['certificate_manager_certificate_authority'] = 'Certificate Authority';
 $lang['certificate_manager_certificate'] = 'Certificate';
