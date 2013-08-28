baa2701d194c6706842dd6a6efe0bf08f7a126cf
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-28 13:55:30 +0000

    - Prepped for 6.5.0 Beta 1 release

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'certificate_manager';
-$app['version'] = '1.4.20';
+$app['version'] = '1.5.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
