32eb440ad6737609000f60a6d97e079555f0cbe5
pcbaldwin <pbaldwin@clearfoundation.com>
2015-04-22 12:01:11 -0400

    Fixed typo in ugprade script

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'certificate_manager';
-$app['version'] = '2.0.20';
+$app['version'] = '2.0.24';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-certificate-manager.spec
+++ packaging/app-certificate-manager.spec
@@ -1,7 +1,7 @@
 
 Name: app-certificate-manager
 Epoch: 1
-Version: 2.0.20
+Version: 2.0.24
 Release: 1%{dist}
 Summary: Certificate Manager
 License: GPLv3
