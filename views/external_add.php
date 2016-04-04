e0b98b65f868f40bc0aed77cda33fbfac1f6e8a0
pcbaldwin <pbaldwin@clearfoundation.com>
2016-04-04 12:35:16 -0400

    Clarify CA file in external certificates [tracker #7941]

--- views/external_add.php
+++ views/external_add.php
@@ -26,9 +26,11 @@ $this->lang->load('certificate_manager');
 echo form_open_multipart('certificate_manager/external/add');
 echo form_header(lang('certificate_manager_add_certificate'));
 
+echo fieldset_header(lang('base_required'));
 echo field_input('name', $name, lang('certificate_manager_name'));
 echo field_file('cert_file', $cert_file, lang('certificate_manager_certificate_file'));
 echo field_file('key_file', $key_file, lang('certificate_manager_key_file'));
+echo fieldset_header(lang('certificate_manager_self_signed_certificate_authority'));
 echo field_file('ca_file', $ca_file, lang('certificate_manager_ca_file'));
 
 echo field_button_set(
