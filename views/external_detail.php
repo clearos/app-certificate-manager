baaea0445768f15f187b1ea66796760da91e3c31
pcbaldwin <peter@egloo.ca>
2017-05-09 21:04:41 -0400

    Re-add file information form, related to [tracker #14881]

--- views/external_detail.php
+++ views/external_detail.php
@@ -25,21 +25,38 @@ if ($errmsg)
     echo infobox_warning(lang('base_warning'), $errmsg);
 
 ///////////////////////////////////////////////////////////////////////////////
+// File information
+///////////////////////////////////////////////////////////////////////////////
+
+echo form_open('certificate_manager/external/view/' . $name);
+echo form_header(lang('certificate_manager_file_information'));
+
+echo field_input('name', $name, lang('certificate_manager_name'), TRUE);
+echo field_input('key', $certificate['key'], lang('certificate_manager_key_file'), TRUE);
+echo field_input('crt', $certificate['crt'], lang('certificate_manager_certificate_file'), TRUE);
+echo field_input('intermediate', $certificate['intermediate'], lang('certificate_manager_intermediate_file'), TRUE);
+
+echo field_button_set(
+    array(
+        anchor_custom('/app/certificate_manager', lang('base_return_to_summary'))
+    )
+);
+
+
+echo form_footer();
+echo form_close();
+
+///////////////////////////////////////////////////////////////////////////////
 // State
 ///////////////////////////////////////////////////////////////////////////////
 
 $items = array();
+$anchors = array();
 
 $headers = array(
     lang('certificate_manager_app'),
     lang('base_details'),
 );
-
-$anchors = anchor_custom(
-    '/app/certificate_manager',
-    lang('base_return_to_summary')
-);
-
 
 foreach ($state as $certificate) {
     $item['title'] = $certificate['description'];
