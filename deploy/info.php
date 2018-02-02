5f71fee2712ba465560312ae926f22b45fa0e874
pcbaldwin <peter@egloo.ca>
2018-02-02 14:18:00 -0500

    Bumped due to buildsys

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'certificate_manager';
-$app['version'] = '2.4.16';
+$app['version'] = '2.4.20';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-certificate-manager.spec
+++ packaging/app-certificate-manager.spec
@@ -1,7 +1,7 @@
 
 Name: app-certificate-manager
 Epoch: 1
-Version: 2.4.16
+Version: 2.4.20
 Release: 1%{dist}
 Summary: Certificate Manager
 License: GPLv3
